a07385dad37b71ba8379d7b62129f3d92eb8814c
Andrew Hejl <andrew.hejl@outlook.com>
2021-02-27 11:00:03 -0800

    Fixed issues

--- Encrypt.cs
+++ Encrypt.cs
@@ -44,7 +44,6 @@ namespace EncryptionProj
 
             Key1 = key;
             Console.WriteLine($"The key for your file is: {key}");
-            Console.WriteLine($"Encrypted data: {strOutput}");
             return strOutput;
         }
         public static string Decrypt(string input, string key)
